d17c82cd4d89147d8f935f726a10b7cff0b11a38
Sergio <sergioescudergallego@enti.cat>
2020-03-09 18:02:18 +0100

    Movement Done/ To Do Animation

---
+++ TheSniffer/Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    #region VARIABLES
+    [SerializeField] float moveSpeed = 5;
+    [SerializeField] Rigidbody2D rb;
+    Vector2 movement;
+    #endregion
+
+    // Update is called once per frame
+    void Update()
+    {
+        //input   
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        //Movement
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
