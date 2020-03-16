88dd0ca78d2138976fff23ad057894769f79c595
Martigomezprunera <magopru1995@gmail.com>
2020-03-16 13:13:17 +0100

    Menu Semidone!

---
+++ TheSniffer/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    //VARIABLES
+    public Slider VolumeSlider;
+
+    //CREAR RANDOM DE MAPA
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("IgnacioScene");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+    public void Volume()
+    {
+        AudioListener.volume = VolumeSlider.value;
+    }
+}
