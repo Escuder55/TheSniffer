35f6d6346b87f5ebacf6c402544310caf9589c03
Ignacio <ignacioperezvelasco@enti.cat>
2020-03-19 16:58:07 +0100

    Prefabs of objects done
    next step: logic of having object

--- TheSniffer/Assets/Scripts/LockerScript.cs
+++ TheSniffer/Assets/Scripts/LockerScript.cs
@@ -5,19 +5,58 @@ using UnityEngine;
 public class LockerScript : MonoBehaviour
 {
     Animator myAnimator;
+    [SerializeField] float maxTime = 2f;
+    float aux = 0f;
+    bool isSelected=false;
+    bool _checked=false;
+    bool hasObject = false;
     // Start is called before the first frame update
     void Start()
     {
         myAnimator = this.GetComponent<Animator>();
     }
 
+    private void FixedUpdate()
+    {
+        if (isSelected && !_checked)
+        {
+            aux += Time.deltaTime;
+            if (aux > maxTime)
+            {
+                isSelected = false;
+                myAnimator.SetBool("Open", true);
+                _checked = true;
+
+                if (hasObject)
+                {
+                    //Spawn Object / give points
+                }
+            }
+        }
+
+        if (hasObject)
+        {
+            //code that modify progressBar
+        }
+    }
+
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.tag == "Player")
         {
-            Debug.Log("Ha entrao nene");
+            isSelected = true;
+            myAnimator.SetBool("Selected",true);
         }
-        Debug.Log("Ha entrao nene");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isSelected = false;
+            myAnimator.SetBool("Selected", false);
+            aux = 0f;
+        }
     }
 
 }
