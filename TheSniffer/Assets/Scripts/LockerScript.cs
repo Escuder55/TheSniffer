d2aea9a88db52d0cf051c4b0d9d6b4f29dbb980e
Ignacio <ignacioperezvelasco@enti.cat>
2020-03-19 12:19:11 +0100

    Sprites and animatorLogic

---
+++ TheSniffer/Assets/Scripts/LockerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockerScript : MonoBehaviour
+{
+    Animator myAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnimator = this.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("Ha entrao nene");
+        }
+        Debug.Log("Ha entrao nene");
+    }
+
+}
