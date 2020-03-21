0fb1a5b7c03baeec08f3092b208c8920e6b3e243
Ignacio <ignacioperezvelasco@enti.cat>
2020-03-21 15:32:06 +0100

    Logic and sprites

--- TheSniffer/Assets/Scripts/Enemy.cs
+++ TheSniffer/Assets/Scripts/Enemy.cs
@@ -11,6 +11,8 @@ public class Enemy : MonoBehaviour
     bool isGoing = false;
     Rigidbody myRb;
     [SerializeField]int indexSpots = 1;
+
+    Animator myAnim;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,12 +30,13 @@ public class Enemy : MonoBehaviour
             default:
                 break;
         }
-        
+        myAnim = this.GetComponent<Animator>();
         idNextSpot = Random.Range(0,(moveSpots.Length - 1));
         nextSpot = moveSpots[idNextSpot].transform;
-        transform.DOMove(nextSpot.transform.position, Random.Range(3,10));        
+        transform.DOMove(nextSpot.transform.position, Random.Range(3,10));
+        selectAnim(this.transform.position, nextSpot.transform.position);
     }
-
+    
     // Update is called once per frame
     void Update()
     {
@@ -42,8 +45,57 @@ public class Enemy : MonoBehaviour
             idNextSpot = Random.Range(0, (moveSpots.Length - 1));
             nextSpot = moveSpots[idNextSpot].transform;
             transform.DOMove(nextSpot.transform.position, Random.Range(3, 10));
+            selectAnim(this.transform.position, nextSpot.transform.position);
         }
     }
 
+
+    #region ANIMATIONCONTROLLER
+    void selectAnim(Vector3 myPos, Vector3 nextPos)
+    {
+        float difY = nextPos.y - myPos.y;
+        float difX = nextPos.x - myPos.x;
+        //case going verticaly
+        if (Mathf.Abs(difX) < Mathf.Abs(difY))
+        {
+            //GoingDown
+            if (difY < 0)
+            {
+                myAnim.SetBool("Up", false);
+                myAnim.SetBool("Right", false);
+                myAnim.SetBool("Left", false);
+                myAnim.SetBool("Down", true);
+            }
+            //GoingUp
+            else
+            {
+                myAnim.SetBool("Right", false);
+                myAnim.SetBool("Left", false);
+                myAnim.SetBool("Down", false);
+                myAnim.SetBool("Up", true);
+            }
+        }
+        //case going horizontally
+        else
+        {
+            //GoingLeft
+            if (difX < 0)
+            {
+                myAnim.SetBool("Right", false);
+                myAnim.SetBool("Down", false);
+                myAnim.SetBool("Up", false);
+                myAnim.SetBool("Left", true);
+            }
+            //GoingRight
+            else
+            {
+                myAnim.SetBool("Down", false);
+                myAnim.SetBool("Up", false);
+                myAnim.SetBool("Left", false);
+                myAnim.SetBool("Right", true);
+            }
+        }
+    }
+    #endregion
 
 }
